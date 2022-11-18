a610c5e883a37e6d98c6ff6e18eff5d334ef9b73
Hilyaa29 <ad2477970@gmail.com>
2022-11-18 10:12:53 +0700

    first commit

---
+++ application/views/admin/index.php
@@ -0,0 +1,9 @@
+<!-- Begin page Content -->
+<div class="container-fluid">
+
+<!-- page heading-->
+<h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+</div>
+</div>
+<!-- End of Main Content -->
